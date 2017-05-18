0bc7a6b8829904b52268372dfa22a86f53a363be
Cristian Camilo Ledesma López <ledesmalopezcristian@gmail.com>
2017-05-18 13:17:42 -0500

    Maquetado

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 10) {
+      $("#encabezado").hide(1000);
+      $(".contenido").css("padding-top","100px");
+    } else {
+      $("#encabezado").show(1000,function() {
+        $(".contenido").css("padding-top","170px");
+      });
+    }
+  });
+
+});
